b7e662ef26738f1681be5cb7487eeda69bc91d0f
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-06-20 19:44:46 +0300

    feat: Add Strapi Algolia plugin for search functionality
    - Add "strapi-plugin-strapi-algolia" npm dependency
    - Update ".env.example" with Algolia configuration variables
    - Update "config/plugins.ts" to enable and configure the Strapi Algolia plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({ env }) => ({
+    // ...
+    'strapi-algolia': {
+        enabled: true,
+        config: {
+            apiKey: env('ALGOLIA_ADMIN_KEY'),
+            applicationId: env('ALGOLIA_APP_ID'),
+            indexPrefix: env('ALGOLIA_INDEX_PREFIX'),
+            contentTypes: [
+                { name: 'api::article.article', index: 'news' },
+                { name: 'api::event.event', index: 'events' },
+                { name: 'api::project.project', index: 'projects' },
+                { name: 'api::publication.publication', index: 'publications' },
+                { name: 'api::priority-area.priority-area', index: 'priority-area' },
+
+            ],
+        },
+    },
+});
